4ddcf2b791cbda98609516c36e8c195a8fd7c1b9
paulbricman <paulbricman@protonmail.com>
2021-10-04 10:44:10 +0200

    Frontend maybe 60% done

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import plotly
 import streamlit as st
 import numpy as np
 import pandas as pd
 import time
 import plotly.express as px
 import plotly.graph_objects as go
+import plotly.offline as pyo
 import datetime
 import calendar
 
@@ -39,11 +41,11 @@ An instrument for quantifying, understanding, and optimizing your thinking, stru
 
 st.markdown('---')
 st.header('🌿 memetics / 🐣 birth rate')
-st.caption('This is the rate at which new ideas saved to your conceptarium.')
+st.caption('This is the rate at which new ideas get saved to your conceptarium.')
 col1, col2, col3, col4 = st.columns(4)
 
 col1.metric(label='birth rate over past day', value='12', delta='+3')
-col2.metric(label='birth rate over past week', value='54', delta='+7')
+col2.metric(label='birth rate over past week', value='54', delta='+6')
 col3.metric(label='birth rate over past month', value='230', delta='-2')
 col4.metric(label='birth rate over past year', value='2498', delta='-342')
 
@@ -98,6 +100,8 @@ st.header('🌿 memetics / 🐇 population size')
 st.caption(
     'This is a measure of how large the ecology of your mind is in terms of individual thoughts.')
 
+col1, col2 = st.columns(2)
+
 memetic_variability_per_month = [
     sum(birth_rate_per_day[e:]) for e in range(len(birth_rate_per_day))]
 fig = px.line(memetic_variability_per_month, color_discrete_sequence=[
@@ -107,18 +111,40 @@ fig.update_xaxes(title_text='days ago', autorange='reversed')
 fig.update_yaxes(title_text='population size')
 fig.update_xaxes(showline=True, linewidth=1, linecolor='#474539', mirror=True)
 fig.update_yaxes(showline=True, linewidth=1, linecolor='#474539', mirror=True)
-st.plotly_chart(fig)
+col1.plotly_chart(fig)
+
+layout = go.Layout(yaxis=go.layout.YAxis(title='age (weeks)', showline=True, linewidth=1, linecolor='#474539', mirror=True),
+                   xaxis=go.layout.XAxis(range=[-10, 10], title='count', showline=True, linewidth=1, linecolor='#474539', mirror=True),
+                   barmode='overlay',
+                   bargap=0.1,
+                   title='population pyramid of fittest quartile')
+
+data = [go.Bar(x=-np.random.rand(10) * list(range(10, 0, -1)),
+               y=list(range(0, 10)),
+               orientation='h',
+               name='language',
+               hoverinfo='x',
+               marker=dict(color='#42D142')
+               ),
+        go.Bar(x=np.random.rand(10) * list(range(10, 0, -1)),
+               y=list(range(0, 10)),
+               orientation='h',
+               name='imagery',
+               marker=dict(color='seagreen')
+               )]
+
+col2.plotly_chart(dict(data=data, layout=layout))
 
 st.markdown('---')
 st.header('🌿 memetics / 🐋 variability')
 st.caption('This is a measure of how diverse your thinking is at any given time, a measure of the biodiversity of your ideas.')
 col1, col2, col3, col4 = st.columns(4)
 
-col1.metric(label='variability over past week', value='54', delta='+7')
+col1.metric(label='variability over past week', value='6.3', delta='+1.2')
 col2.metric(label='variability over past month',
-            value='230', delta='-2')
-col3.metric(label='aggregate variability', value='12')
-col4.metric(label='variability of fittest quartile', value='12')
+            value='4.6', delta='-0.5')
+col3.metric(label='aggregate variability', value='5.8')
+col4.metric(label='variability of fittest quartile', value='2.3')
 
 
 col1, col2 = st.columns(2)
@@ -216,7 +242,109 @@ fig.update_xaxes(showline=True, linewidth=1, linecolor='#474539', mirror=True)
 fig.update_yaxes(showline=True, linewidth=1, linecolor='#474539', mirror=True)
 col2.plotly_chart(fig)
 
+st.markdown('---')
+st.header('📗 linguistics / ⏱️ conciseness')
+st.caption(
+    'Conciseness is based on the length of language thoughts.')
+col1, col2 = st.columns(2)
 
+memetic_variability_per_week = np.random.uniform(5, 3, (20))
+fig = px.line(memetic_variability_per_week, color_discrete_sequence=[
+    '#228b22'], title='conciseness per week', line_shape='spline')
+fig.update_layout(showlegend=False)
+fig.update_xaxes(title_text='weeks ago', autorange='reversed')
+fig.update_yaxes(title_text='conciseness')
+fig.update_xaxes(showline=True, linewidth=1, linecolor='#474539', mirror=True)
+fig.update_yaxes(showline=True, linewidth=1, linecolor='#474539', mirror=True)
+col1.plotly_chart(fig)
+
+weekly_discovery_rates = np.abs(np.random.normal(0.1, 0.05, 20))
+fig = px.box(weekly_discovery_rates, color_discrete_sequence=[
+    '#228b22'], title='conciseness distribution over past month')
+fig.update_layout(showlegend=False)
+fig.update_xaxes(title_text='')
+fig.update_yaxes(title_text='conciseness')
+fig.update_xaxes(showline=True, linewidth=1, linecolor='#474539', mirror=True)
+fig.update_yaxes(showline=True, linewidth=1, linecolor='#474539', mirror=True)
+col2.plotly_chart(fig)
+
+st.markdown('---')
+st.header('📗 linguistics / 📰 readability')
+st.caption(
+    'Readability is based on the average word length and average sentence length.')
+col1, col2 = st.columns(2)
+
+memetic_variability_per_week = np.random.uniform(5, 3, (20))
+fig = px.line(memetic_variability_per_week, color_discrete_sequence=[
+    '#228b22'], title='readability per week', line_shape='spline')
+fig.update_layout(showlegend=False)
+fig.update_xaxes(title_text='weeks ago', autorange='reversed')
+fig.update_yaxes(title_text='readability')
+fig.update_xaxes(showline=True, linewidth=1, linecolor='#474539', mirror=True)
+fig.update_yaxes(showline=True, linewidth=1, linecolor='#474539', mirror=True)
+col1.plotly_chart(fig)
+
+weekly_discovery_rates = np.abs(np.random.normal(0.1, 0.05, 20))
+fig = px.box(weekly_discovery_rates, color_discrete_sequence=[
+    '#228b22'], title='readability distribution over past month')
+fig.update_layout(showlegend=False)
+fig.update_xaxes(title_text='')
+fig.update_yaxes(title_text='readability')
+fig.update_xaxes(showline=True, linewidth=1, linecolor='#474539', mirror=True)
+fig.update_yaxes(showline=True, linewidth=1, linecolor='#474539', mirror=True)
+col2.plotly_chart(fig)
+
+st.markdown('---')
+st.header('📗 linguistics / 📏 objectivity')
+st.caption(
+    'Objectivity is a measure of how much your language thoughts appear to describe facts, rather than opinions.')
+col1, col2 = st.columns(2)
+
+memetic_variability_per_week = np.random.uniform(5, 3, (20))
+fig = px.line(memetic_variability_per_week, color_discrete_sequence=[
+    '#228b22'], title='objectivity per week', line_shape='spline')
+fig.update_layout(showlegend=False)
+fig.update_xaxes(title_text='weeks ago', autorange='reversed')
+fig.update_yaxes(title_text='objectivity')
+fig.update_xaxes(showline=True, linewidth=1, linecolor='#474539', mirror=True)
+fig.update_yaxes(showline=True, linewidth=1, linecolor='#474539', mirror=True)
+col1.plotly_chart(fig)
+
+weekly_discovery_rates = np.abs(np.random.normal(0.1, 0.05, 20))
+fig = px.box(weekly_discovery_rates, color_discrete_sequence=[
+    '#228b22'], title='objectivity distribution over past month')
+fig.update_layout(showlegend=False)
+fig.update_xaxes(title_text='')
+fig.update_yaxes(title_text='objectivity')
+fig.update_xaxes(showline=True, linewidth=1, linecolor='#474539', mirror=True)
+fig.update_yaxes(showline=True, linewidth=1, linecolor='#474539', mirror=True)
+col2.plotly_chart(fig)
+
+st.markdown('---')
+st.header('📗 linguistics / 💚 sentiment')
+st.caption(
+    'Sentiment is a measure of how positive your language thoughts are.')
+col1, col2 = st.columns(2)
+
+memetic_variability_per_week = np.random.uniform(5, 3, (20))
+fig = px.line(memetic_variability_per_week, color_discrete_sequence=[
+    '#228b22'], title='sentiment per week', line_shape='spline')
+fig.update_layout(showlegend=False)
+fig.update_xaxes(title_text='weeks ago', autorange='reversed')
+fig.update_yaxes(title_text='sentiment')
+fig.update_xaxes(showline=True, linewidth=1, linecolor='#474539', mirror=True)
+fig.update_yaxes(showline=True, linewidth=1, linecolor='#474539', mirror=True)
+col1.plotly_chart(fig)
+
+weekly_discovery_rates = np.abs(np.random.normal(0.1, 0.05, 20))
+fig = px.box(weekly_discovery_rates, color_discrete_sequence=[
+    '#228b22'], title='sentiment distribution over past month')
+fig.update_layout(showlegend=False)
+fig.update_xaxes(title_text='')
+fig.update_yaxes(title_text='sentiment')
+fig.update_xaxes(showline=True, linewidth=1, linecolor='#474539', mirror=True)
+fig.update_yaxes(showline=True, linewidth=1, linecolor='#474539', mirror=True)
+col2.plotly_chart(fig)
 
 st.markdown('---')
 st.header('🖼️ semantics / 🔭 discovery')
@@ -224,13 +352,13 @@ st.caption('This is a measure of how much of the semantic space you\'ve explored
 
 col1, col2, col3, col4 = st.columns(4)
 
-col1.metric(label='semantic dicovery over past week', value='54', delta='+7')
-col2.metric(label='semantic dicovery over past month',
-            value='12%', delta='3%')
+col1.metric(label='dicovery over past week', value='0.21%', delta='+0.02%')
+col2.metric(label='dicovery over past month',
+            value='0.87%', delta='-0.09%')
 col3.metric(label='explored proportion of semantic volume',
-            value='230', delta='-2')
+            value='12.52%')
 col4.metric(label='ETA complete semantic coverage',
-            value='12', delta='3%')
+            value='67.3 YRS')
 
 col1, col2 = st.columns(2)
 
@@ -278,71 +406,3 @@ fig.update_yaxes(title_text='explored proportion of semantic volume')
 fig.update_xaxes(showline=True, linewidth=1, linecolor='#474539', mirror=True)
 fig.update_yaxes(showline=True, linewidth=1, linecolor='#474539', mirror=True)
 col1.plotly_chart(fig)
-
-embeddings = [np.append(e, 0.5) for e in np.random.rand(300, 3)]
-fig = px.scatter_3d(pd.DataFrame(embeddings, columns=['x', 'y', 'z', 'size']), x='x', y='y', z='z', size='size', size_max=5, color_discrete_sequence=[
-    '#228b22'], title='low-dimensional projection')
-fig.update_layout(showlegend=False, margin=dict(l=0, r=0, b=0), xaxis_range=[-10, 10], xaxis_autorange=False)
-fig.update_xaxes(title_text='weeks ago', autorange=False, range=[-10, 10])
-fig.update_yaxes(title_text='explored proportion of semantic volume')
-fig.update_xaxes(showline=True, linewidth=1, linecolor='#474539', mirror=True)
-fig.update_yaxes(showline=True, linewidth=1, linecolor='#474539', mirror=True)
-col2.plotly_chart(fig)
-
-st.markdown('---')
-st.header('📗 linguistics')
-st.caption(
-    'This family of measures specifically cover language.')
-col1, col2, col3, col4 = st.columns(4)
-
-col1.metric(label='conciseness over past week', value='54', delta='+7')
-col2.metric(label='readability over past week',
-            value='12%', delta='3%')
-col3.metric(label='objectivity over past week',
-            value='230', delta='-2')
-col4.metric(label='sentiment over past week',
-            value='12', delta='3%')
-
-
-col1, col2 = st.columns(2)
-memetic_variability_per_week = np.random.uniform(5, 3, (20))
-fig = px.line(memetic_variability_per_week, color_discrete_sequence=[
-    '#228b22'], title='drift per week', line_shape='spline')
-fig.update_layout(showlegend=False)
-fig.update_xaxes(title_text='weeks ago', autorange='reversed')
-fig.update_yaxes(title_text='drift')
-fig.update_xaxes(showline=True, linewidth=1, linecolor='#474539', mirror=True)
-fig.update_yaxes(showline=True, linewidth=1, linecolor='#474539', mirror=True)
-col1.plotly_chart(fig)
-
-memetic_variability_per_month = np.random.uniform(5, 3, (5))
-memetic_variability_per_week = np.random.uniform(5, 3, (20))
-fig = px.line(memetic_variability_per_week, color_discrete_sequence=[
-    '#228b22'], title='drift per week', line_shape='spline')
-fig.update_layout(showlegend=False)
-fig.update_xaxes(title_text='weeks ago', autorange='reversed')
-fig.update_yaxes(title_text='drift')
-fig.update_xaxes(showline=True, linewidth=1, linecolor='#474539', mirror=True)
-fig.update_yaxes(showline=True, linewidth=1, linecolor='#474539', mirror=True)
-col2.plotly_chart(fig)
-
-memetic_variability_per_week = np.random.uniform(5, 3, (20))
-fig = px.line(memetic_variability_per_week, color_discrete_sequence=[
-    '#228b22'], title='drift per week', line_shape='spline')
-fig.update_layout(showlegend=False)
-fig.update_xaxes(title_text='weeks ago', autorange='reversed')
-fig.update_yaxes(title_text='drift')
-fig.update_xaxes(showline=True, linewidth=1, linecolor='#474539', mirror=True)
-fig.update_yaxes(showline=True, linewidth=1, linecolor='#474539', mirror=True)
-col1.plotly_chart(fig)
-
-memetic_variability_per_month = np.random.uniform(5, 3, (5))
-memetic_variability_per_week = np.random.uniform(5, 3, (20))
-fig = px.line(memetic_variability_per_week, color_discrete_sequence=[
-    '#228b22'], title='drift per week', line_shape='spline')
-fig.update_layout(showlegend=False)
-fig.update_xaxes(title_text='weeks ago', autorange='reversed')
-fig.update_yaxes(title_text='drift')
-fig.update_xaxes(showline=True, linewidth=1, linecolor='#474539', mirror=True)
-fig.update_yaxes(showline=True, linewidth=1, linecolor='#474539', mirror=True)
-col2.plotly_chart(fig)
