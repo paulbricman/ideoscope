00b4e4263c5f2844d8e7a686f5b417288c48e7cf
paulbricman <paulbricman@protonmail.com>
2021-10-08 12:08:15 +0200

    Connect data to birth_rate_subsection(), population_size_subsection()

--- components.py
+++ components.py
@@ -4,20 +4,23 @@ import plotly.express as px
 import plotly.graph_objects as go
 import calendar
 import streamlit as st
-
+from data import *
 
 def sidebar_section():
     st.sidebar.header('settings')
     st.session_state.conceptarium_url = st.sidebar.text_input(
         'What\'s the URL of your conceptarium?')
-    st.session_state.timezone = st.sidebar.number_input(
+    
+    timezone = st.sidebar.number_input(
         'What timezone are you in? (UTC+...)', step=1)
-
+    if timezone >= 0:
+        st.session_state.timezone = 'Etc/GMT-' + str(int(abs(timezone)))
+    else:
+        st.session_state.timezone = 'Etc/GMT+' + str(int(abs(timezone)))
 
 def header_section():
     st.title('🔬 ideoscope')
-    st.markdown(
-        'An instrument for quantifying, understanding, and optimizing your thinking, split into three sections:')
+    st.markdown('An instrument for quantifying, understanding, and optimizing your thinking, split into three sections:')
 
     col1, col2, col3 = st.columns(3)
     col1.markdown('''#### 🌿 memetics
@@ -54,105 +57,89 @@ def birth_rate_subsection():
     st.caption('The rate at which new ideas get saved to your conceptarium.')
     col1, col2, col3, col4 = st.columns(4)
 
-    col1.metric(label='birth rate over past day', value='12', delta='+3')
-    col2.metric(label='birth rate over past week', value='54', delta='+6')
-    col3.metric(label='birth rate over past month', value='230', delta='-2')
-    col4.metric(label='birth rate over past year', value='2498', delta='-342')
+    value, delta = birth_rate_over_past_day()
+    col1.metric(label='birth rate over past day', value=value, delta=delta)
+
+    value, delta = birth_rate_over_past_week()
+    col2.metric(label='birth rate over past week', value=value, delta=delta)
+
+    value, delta = birth_rate_over_past_month()
+    col3.metric(label='birth rate over past month', value=value, delta=delta)
+
+    value, delta = birth_rate_over_past_year()
+    col4.metric(label='birth rate over past year', value=value, delta=delta)
 
     col1, col2 = st.columns(2)
 
-    birth_rate_per_day = np.random.randint(0, 20, (100))
-    fig = px.line(birth_rate_per_day, color_discrete_sequence=[
-        '#228b22'], title='daily birth rate', line_shape='spline')
+    data = daily_birth_rate()
+    fig = px.line(data, color_discrete_sequence=['#228b22'], title='daily birth rate', line_shape='spline')
     fig.update_layout(showlegend=False)
     fig.update_xaxes(title_text='days ago', autorange='reversed')
     fig.update_yaxes(title_text='birth rate')
-    fig.update_xaxes(showline=True, linewidth=1,
-                     linecolor='#474539', mirror=True)
-    fig.update_yaxes(showline=True, linewidth=1,
-                     linecolor='#474539', mirror=True)
+    fig.update_xaxes(showline=True, linewidth=1, linecolor='#474539', mirror=True)
+    fig.update_yaxes(showline=True, linewidth=1, linecolor='#474539', mirror=True)
     col1.plotly_chart(fig)
 
-    birth_rate_by_day_of_the_week = np.random.choice(
-        calendar.day_abbr[0:7], 200)
-    df = pd.DataFrame(birth_rate_by_day_of_the_week, columns=['Time'])
-    fig = px.histogram(df, x='Time', nbins=12, color_discrete_sequence=[
-        '#228b22'], labels={'count': '', 'Time': ''}, category_orders={'Time': calendar.day_abbr[0:7]}, title='birth rate by day of the week')
+    
+    data = birth_rate_by_day_of_week()
+    fig = px.histogram(data, x='weekday', nbins=12, color_discrete_sequence=['#228b22'], category_orders={'weekday': calendar.day_abbr[0:7]}, title='birth rate by day of the week')
     fig.update_layout(bargap=0.2)
     fig.update_xaxes(title_text='day of the week')
     fig.update_yaxes(title_text='birth rate')
-    fig.update_xaxes(showline=True, linewidth=1,
-                     linecolor='#474539', mirror=True)
-    fig.update_yaxes(showline=True, linewidth=1,
-                     linecolor='#474539', mirror=True)
+    fig.update_xaxes(showline=True, linewidth=1, linecolor='#474539', mirror=True)
+    fig.update_yaxes(showline=True, linewidth=1, linecolor='#474539', mirror=True)
     col2.plotly_chart(fig)
 
-    birth_rate_by_time_of_day = pd.DataFrame(
-        np.random.normal(1600, 150, 200))
-    fig = px.histogram(birth_rate_by_time_of_day, nbins=12, color_discrete_sequence=[
-        '#228b22'], labels={'count': '', 'Time': ''}, title='birth rate by time of day')
+    data = birth_rate_by_time_of_day()
+    fig = px.histogram(data, nbins=12, color_discrete_sequence=['#228b22'], title='birth rate by time of day')
     fig.update_layout(bargap=0.2, showlegend=False)
-    fig.update_xaxes(title_text='time of day')
+    fig.update_xaxes(title_text='time of day', tickformat='%H:%M')
     fig.update_yaxes(title_text='birth rate')
-    fig.update_xaxes(showline=True, linewidth=1,
-                     linecolor='#474539', mirror=True)
-    fig.update_yaxes(showline=True, linewidth=1,
-                     linecolor='#474539', mirror=True)
+    fig.update_xaxes(showline=True, linewidth=1, linecolor='#474539', mirror=True)
+    fig.update_yaxes(showline=True, linewidth=1, linecolor='#474539', mirror=True)
     col1.plotly_chart(fig)
 
-    birth_rate_by_month = np.random.choice(calendar.month_abbr[1:13], 200)
-    df = pd.DataFrame([e for e in zip(birth_rate_by_day_of_the_week, np.random.normal(1600, 200, 200))],
-                      columns=['weekday', 'hour'])
-    fig = px.density_heatmap(df, x='weekday', y='hour', category_orders={
-        'weekday': calendar.day_abbr[0:7]}, color_continuous_scale=['#fffffd', '#228b22'], title='birth rate by day of the week and time of day')
+    data = birth_rate_by_time_of_day_and_day_of_week()
+    fig = px.density_heatmap(data, x='weekday', y='time', category_orders={'weekday': calendar.day_abbr[0:7]}, color_continuous_scale=['#fffffd', '#228b22'], title='birth rate by day of the week and time of day')
     fig.update_layout(bargap=0.2, xaxis={'side': 'top'})
     fig.update_xaxes(title_text='')
-    fig.update_yaxes(title_text='time of day', autorange='reversed')
-    fig.update_xaxes(showline=True, linewidth=1,
-                     linecolor='#474539', mirror=True)
-    fig.update_yaxes(showline=True, linewidth=1,
-                     linecolor='#474539', mirror=True)
+    fig.update_yaxes(title_text='time of day', autorange='reversed', tickformat='%H:%M')
+    fig.update_xaxes(showline=True, linewidth=1, linecolor='#474539', mirror=True)
+    fig.update_yaxes(showline=True, linewidth=1, linecolor='#474539', mirror=True)
     col2.plotly_chart(fig)
-
 
 def population_size_subsection():
     st.markdown('---')
     st.header('🌿 memetics / 🐇 population size')
-    st.caption(
-        'A measure of how large the ecology of your mind is in terms of individual thoughts.')
+    st.caption('A measure of how large the ecology of your mind is in terms of individual thoughts.')
 
     col1, col2 = st.columns(2)
 
-    birth_rate_per_day = np.random.randint(0, 20, (100))
-    memetic_variability_per_month = [
-        sum(birth_rate_per_day[e:]) for e in range(len(birth_rate_per_day))]
-    fig = px.line(memetic_variability_per_month, color_discrete_sequence=[
-        '#228b22'], title='population size per day', line_shape='spline')
+    data = population_size_per_day()
+    fig = px.line(data, color_discrete_sequence=['#228b22'], title='population size per day', line_shape='spline')
     fig.update_layout(showlegend=False)
     fig.update_xaxes(title_text='days ago', autorange='reversed')
     fig.update_yaxes(title_text='population size')
-    fig.update_xaxes(showline=True, linewidth=1,
-                     linecolor='#474539', mirror=True)
-    fig.update_yaxes(showline=True, linewidth=1,
-                     linecolor='#474539', mirror=True)
+    fig.update_xaxes(showline=True, linewidth=1, linecolor='#474539', mirror=True)
+    fig.update_yaxes(showline=True, linewidth=1, linecolor='#474539', mirror=True)
     col1.plotly_chart(fig)
 
+    data = population_pyramid_of_fittest_quartile()
     layout = go.Layout(yaxis=go.layout.YAxis(title='age (weeks)', showline=True, linewidth=1, linecolor='#474539', mirror=True),
                        xaxis=go.layout.XAxis(
                            range=[-15, 15], title='count', showline=True, linewidth=1, linecolor='#474539', mirror=True),
                        barmode='overlay',
                        bargap=0.1,
                        title='population pyramid of fittest quartile')
-
-    data = [go.Bar(x=-(0.5+0.5*np.random.rand(10)) * list(range(10, 0, -1)),
-                   y=list(range(0, 10)),
+    data = [go.Bar(x=data[0],
+                   y=list(range(len(data[0]))),
                    orientation='h',
                    name='language',
                    hoverinfo='x',
                    marker=dict(color='#42D142')
                    ),
-            go.Bar(x=(0.5+0.5*np.random.rand(10)) * list(range(10, 0, -1)),
-                   y=list(range(0, 10)),
+            go.Bar(x=data[1],
+                   y=list(range(len(data[1]))),
                    orientation='h',
                    name='imagery',
                    marker=dict(color='seagreen')
--- data.py
+++ data.py
@@ -1,6 +1,13 @@
+import calendar
+from numpy import empty
 import streamlit as st
 import requests
 import json
+import pandas as pd
+from time import time as now
+from datetime import datetime, date, tzinfo, time
+from pytz import timezone
+import math
 
 
 def fetch_conceptarium():
@@ -12,3 +19,91 @@ def fetch_conceptarium():
     # conceptarium_json = requests.get(conceptarium_url).json()
     # st.session_state.conceptarium_json = conceptarium_json
     st.session_state.conceptarium_json = json.load(open('dummy.json'))
+
+
+def birth_rate_over_past_day():
+    data = daily_birth_rate()
+    return data[0], data[0] - data[1]
+
+
+def birth_rate_over_past_week():
+    data = daily_birth_rate()
+    return sum(data[:7]), sum(data[:7]) - sum(data[7:14])
+
+
+def birth_rate_over_past_month():
+    data = daily_birth_rate()
+    return sum(data[:30]), sum(data[:30]) - sum(data[30:60])
+
+
+def birth_rate_over_past_year():
+    data = daily_birth_rate()
+    return sum(data[:365]), sum(data[:365]) - sum(data[365:2 * 365])
+
+
+def daily_birth_rate():
+    conceptarium_json = st.session_state.conceptarium_json
+    timestamps = pd.DataFrame(conceptarium_json)['timestamp'].values
+    midnight = datetime.combine(datetime.today(), time.min).timestamp()
+    
+    timestamps = [midnight - e for e in timestamps]
+    timestamps = [int(1 + e / (60 * 60 * 24)) for e in timestamps]
+    timestamps = sorted(timestamps)
+    data = [timestamps.count(e) for e in range(max(timestamps) + 1)]
+    return data
+
+    
+def birth_rate_by_day_of_week():
+    conceptarium_json = st.session_state.conceptarium_json
+    timestamps = pd.DataFrame(conceptarium_json)['timestamp'].values
+
+    data = [date.fromtimestamp(e).strftime('%a') for e in timestamps]
+    return pd.DataFrame(data, columns=['weekday'])
+
+
+def birth_rate_by_time_of_day():
+    conceptarium_json = st.session_state.conceptarium_json
+    timestamps = pd.DataFrame(conceptarium_json)['timestamp'].values
+
+    data = [datetime.fromtimestamp(e, tz=timezone(st.session_state.timezone)).strftime('%H:%M') for e in timestamps]
+    data = pd.to_datetime(data, format='%H:%M')
+    return pd.DataFrame(data, columns=['time'])
+
+
+def birth_rate_by_time_of_day_and_day_of_week():
+    time = birth_rate_by_time_of_day()
+    weekday = birth_rate_by_day_of_week()
+
+    data = pd.DataFrame()
+    data['time'] = time['time'].values
+    data['weekday'] = weekday['weekday'].values
+    return data
+
+
+def population_size_per_day():
+    data = daily_birth_rate()
+    data = [sum(data[e:]) for e in range(len(data))]
+    return data
+
+
+def population_pyramid_of_fittest_quartile():
+    conceptarium_json = st.session_state.conceptarium_json
+    conceptarium_json = sorted(conceptarium_json, key=lambda x: x['activation'])
+    fittest = conceptarium_json[:math.ceil(len(conceptarium_json) * 0.25)]
+
+    fittest_language = [e for e in fittest if e['modality'] == 'language']
+    fittest_imagery = [e for e in fittest if e['modality'] == 'imagery']
+
+    if len(fittest_language) > 0:
+        fittest_language_age = [int((now() - e['timestamp']) / (60 * 60 * 24 * 7)) for e in fittest_language]
+        fittest_language_age = [fittest_language_age.count(e) for e in range(max(fittest_language_age) + 1)]
+    else:
+        fittest_language_age = []
+
+    if len(fittest_imagery) > 0:
+        fittest_imagery_age = [int((now() - e['timestamp']) / (60 * 60 * 24 * 7)) for e in fittest_imagery]
+        fittest_imagery_age = [fittest_imagery_age.count(e) for e in range(max(fittest_imagery_age) + 1)]
+    else:
+        fittest_imagery_age = []
+
+    return fittest_language_age, fittest_imagery_age
